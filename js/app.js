1587208b5fd27f45010ba7345f21981ca1662cf9
Summer S <snowykittin@gmail.com>
2022-09-07 17:22:18 -0400

    updated for callbacks

--- js/app.js
+++ js/app.js
@@ -1,17 +1,9 @@
-// //create a synth and connect it to the main output (your speakers)
-// let synthApp = {
-//     synth: new Tone.Synth().toDestination(),
-//     notes: [ "C4", "D4", "E4", "F4"],
-//     currentNote: 0,
-//     playSound(){
-//         this.synth.triggerAttackRelease(this.notes[this.currentNote], "8n");
-//         this.currentNote++;
-
-//         setTimeout(this.playSound.bind(this), 500);
-//     }
-// }
+/*Summer Sexton
+N320
+September 7, 2022*/
 
 let currentInstrument = 0;
+
 
 class Instruments{
     constructor(loudness, family, play){
@@ -20,14 +12,16 @@ class Instruments{
         this.play = play;
     }
     
+    
     synth = new Tone.Synth().toDestination();
-    playSound(duration, over){
-        this.synth.triggerAttack("A4", 0);
-        console.log(this.family + " " + this.play + " at " + this.loudness);
-        over(duration);        
+    over(){
+        this.synth.triggerRelease(0, duration);
+        this.activatedCallback();
     }
-    over(duration){
-        this.synth.triggerRelease(0 + duration);
+    playSound(duration){
+        this.synth.triggerAttackRelease("A4", duration);
+        console.log(this.family + " " + this.play + " at " + this.loudness);
+        this.activatedCallback = this.over;
     }
 }
 
@@ -56,19 +50,20 @@ let flute = new Woodwind("50 db");
 let violin = new String("40 db");
 let drum = new Percussion("80 db");
 
+var instrumentArray = [flute, violin, drum];
 
-var instrumentArray = [flute, violin, drum]
 
-
-for(currentInstrument; currentInstrument<instrumentTest.length; playNext){
-    playNext(currentInstrument);
+function playNext(){
+    instrumentArray[currentInstrument].playSound(0.5);
     if(currentInstrument == 2){
         currentInstrument = 0;
     }else{
-        currentInstrument++;
+       currentInstrument++; 
     }
+    
+    //this.activatedCallback();
 }
 
-function playNext(currentInstrument){
-    instrumentArray[currentInstrument].playSound(0.5);
+for(let i = 0; i < 2; i++){
+    this.activatedCallback = this.playNext;
 }
